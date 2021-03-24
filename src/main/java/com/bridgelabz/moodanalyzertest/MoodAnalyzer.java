d4eb20cf55d089835bf78832274787848a3d3940
MasterX01 <aakashsaxena14@gmail.com>
2021-03-24 15:12:20 +0530

    [Akash] ADD. working UC-1 code

--- src/main/java/com/bridgelabz/moodanalyzertest/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzertest/MoodAnalyzer.java
@@ -2,6 +2,14 @@ package com.bridgelabz.moodanalyzertest;
 
 public class MoodAnalyzer {
 
+    public String analyseMood(String message)  {
+        if (message.contains("Sad")) {
+            return "SAD";
+        }else {
+            return "HAPPY";
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyzer");
     }
---
+++ src/test/java/com/bridgelabz/moodanalyzertest/MoodAnalyzerTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.moodanalyzertest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class MoodAnalyzerTest {
+
+    @Test
+    public void givenMessage_whenSad_ShouldReturnSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyseMood("This is a Sad Message");
+        Assertions.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_whenNotSad_ShouldReturnHappy()  {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyseMood("This is a Happy Message");
+        Assertions.assertEquals("HAPPY", mood);
+    }
+
+}
